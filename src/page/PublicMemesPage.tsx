e803cf141352994b7f790cd948f68c69f240c0cf
JuppLCD <lorenzocanovas207@gmail.com>
2022-09-19 12:16:48 -0300

    Use services/MemesBackend hooks to get user and public memes, removing useUserMemes hook

--- src/page/PublicMemesPage.tsx
+++ src/page/PublicMemesPage.tsx
@@ -1,17 +1,23 @@
-import useUsersMemes from '../hooks/useUsersMemes';
+import { useGetPublicMemesQuery } from '../services/MemesBackend';
 
 import Container from '../components/Container';
 import GridMemes from '../components/GridMemes';
-
 import Meme from '../components/Meme';
+import Spinner from '../components/Spinner';
+import AlertError from '../components/Alerts/Error';
 
 function PublicMemesPage() {
-	const memes = useUsersMemes('/public');
+	const { data: memes, isLoading, isError } = useGetPublicMemesQuery(undefined);
+
+	const hasMeme = memes !== undefined && memes.length !== 0;
+
 	return (
 		<main>
 			<Container>
 				<h2 className='text-center text-2xl mb-7'>Public Memes</h2>
 				<div>
+					{isLoading && <Spinner />}
+
 					{memes !== undefined && memes.length !== 0 ? (
 						<GridMemes>
 							{memes.map((meme) => (
@@ -21,6 +27,8 @@ function PublicMemesPage() {
 					) : (
 						<p>No hay memes</p>
 					)}
+
+					{isError && <AlertError title='Error fetching data' message='Could not get public memes from server' />}
 				</div>
 			</Container>
 		</main>
--- src/page/UserPage.tsx
+++ src/page/UserPage.tsx
@@ -1,17 +1,21 @@
-import useUsersMemes from '../hooks/useUsersMemes';
+import { useGetUserMemesQuery } from '../services/MemesBackend';
 
 import Container from '../components/Container';
 import GridMemes from '../components/GridMemes';
-
 import Meme from '../components/Meme';
+import AlertError from '../components/Alerts/Error';
+import Spinner from '../components/Spinner';
 
 function UserPage() {
-	const memes = useUsersMemes('/');
+	const { data: memes, isLoading, isError } = useGetUserMemesQuery(undefined);
+
 	return (
 		<main>
 			<Container>
 				<h2 className='text-center text-2xl mb-7'>User Memes</h2>
 				<div>
+					{isLoading && <Spinner />}
+
 					{memes !== undefined && memes.length !== 0 ? (
 						<GridMemes>
 							{memes.map((meme) => (
@@ -21,6 +25,8 @@ function UserPage() {
 					) : (
 						<p>No hay memes</p>
 					)}
+
+					{isError && <AlertError title='Error fetching data' message='Could not get public memes from server' />}
 				</div>
 			</Container>
 		</main>
